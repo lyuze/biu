5257b338489cf0d4ee3a3af7f8e405b2205098d9
lyz <253630756@qq.com>
2019-08-13 16:04:46 +0800

    first commit

---
+++ server/src/main/java/com/abc/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.abc.service.impl;
+
+import com.abc.dao.SysUserMapper;
+import com.abc.entity.SysUser;
+import com.abc.service.SysUserService;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> implements SysUserService {
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Page<SysUser> queryUserIncludeRoles(Page page, String nick) {
+        return page.setRecords(baseMapper.selectUserIncludeRoles(page,nick));
+    }
+
+}
